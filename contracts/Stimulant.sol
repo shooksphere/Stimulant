a45ffdd4c2a14ded9b22bb9d71d48452e58bfb42
shooksphere <mr.johnkelsey@gmail.com>
2026-07-14 11:58:50 -0400

    Add mainnet approval transaction hashes and set owner address to 0x0311502EA9AcF3a532d32C8D8830839Ce34bD378

--- contracts/Stimulant.sol
+++ contracts/Stimulant.sol
@@ -16,14 +16,21 @@ contract Stimulant is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
     event TokensMinted(address indexed to, uint256 amount);
     event TokensBurned(address indexed from, uint256 amount);
 
+    // Mainnet approval transaction hashes
+    string public constant APPROVAL_TX_1 = "0x6387457c5600500934253031a1356f8c61f48ed9d891da1385551cde629c1181";
+    string public constant APPROVAL_TX_2 = "0x050c0e2945e206fcc715008e051e2bf78e7a5e8b624874324a590c8727dd72e5";
+    
+    // Designated owner address
+    address public constant DESIGNATED_OWNER = 0x0311502EA9AcF3a532d32C8D8830839Ce34bD378;
+
     /**
      * @dev Constructor to initialize the token
      * @param initialSupply Initial number of tokens to mint (in wei)
      */
-    constructor(uint256 initialSupply) ERC20("Stimulant", "STIM") Ownable(msg.sender) {
+    constructor(uint256 initialSupply) ERC20("Stimulant", "STIM") Ownable(DESIGNATED_OWNER) {
         require(initialSupply > 0, "Initial supply must be greater than 0");
-        _mint(msg.sender, initialSupply);
-        emit TokensMinted(msg.sender, initialSupply);
+        _mint(DESIGNATED_OWNER, initialSupply);
+        emit TokensMinted(DESIGNATED_OWNER, initialSupply);
     }
 
     /**
@@ -86,6 +93,19 @@ contract Stimulant is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
         )
     {
         return (name(), symbol(), decimals(), totalSupply());
+    }
+
+    /**
+     * @dev Get approval transaction hashes
+     * @return tx1 First approval transaction hash
+     * @return tx2 Second approval transaction hash
+     */
+    function getApprovalTransactions()
+        public
+        pure
+        returns (string memory tx1, string memory tx2)
+    {
+        return (APPROVAL_TX_1, APPROVAL_TX_2);
     }
 
     // Required function overrides for ERC20 and ERC20Pausable
