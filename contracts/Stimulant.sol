8d6f7c3e7175f855e0d1c7afb72dea085e8b8ad6
shooksphere <mr.johnkelsey@gmail.com>
2026-07-14 12:07:06 -0400

    Update Stimulant.sol

--- contracts/Stimulant.sol
+++ contracts/Stimulant.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
